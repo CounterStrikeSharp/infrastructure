0fbd45bc4dca3072a4e004992b343d727045aba5
KillStr3aK <KillStr3aK@users.noreply.github.com>
2024-04-29 04:18:02 +0200

    feat: create gdc repo

--- main.tf
+++ main.tf
@@ -90,3 +90,37 @@ resource "github_branch_protection" "docs" {
     restrict_dismissals             = false
   }
 }
+
+resource "github_repository" "gdc" {
+  name       = "GDC"
+  visibility = "public"
+
+  allow_auto_merge       = true
+  allow_merge_commit     = false
+  allow_rebase_merge     = false
+  delete_branch_on_merge = true
+
+  description = "GDC for CounterStrikeSharp"
+
+  has_issues = true
+
+  squash_merge_commit_message = "BLANK"
+  squash_merge_commit_title   = "PR_TITLE"
+}
+
+resource "github_branch_protection" "gdc" {
+  repository_id        = github_repository.gdc.id
+  pattern              = "main"
+  force_push_bypassers = ["/roflmuffin"]
+  push_restrictions    = []
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = true
+    dismissal_restrictions          = []
+    pull_request_bypassers          = []
+    require_code_owner_reviews      = true
+    require_last_push_approval      = false
+    required_approving_review_count = 1
+    restrict_dismissals             = false
+  }
+}
